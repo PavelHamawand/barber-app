bd1a20352f7feadac7f595fd9d45b324246342fb
Pavel Hamawand <Pavelhamawand@gmail.com>
2025-04-09 19:07:56 +0200

    secure supabase integration

---
+++ app.config.js
@@ -0,0 +1,48 @@
+// app.config.js
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: "barber-app",
+    slug: "barber-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      }
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff"
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true
+    },
+    extra: {
+      expoPublicSupabaseUrl: process.env.SUPABASE_URL,
+      expoPublicSupabaseAnonKey: process.env.SUPABASE_ANON_KEY
+    }
+  }
+};
